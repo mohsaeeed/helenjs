256a8becd120c8939a86214162eeedec910c9a29
mohsaeeed <moh.saeeed@gmail.com>
2019-08-26 15:58:44 -0500

    added cli laydown

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-export const Hello = (name: string) => `Hello ${name}`;
+#!/usr/bin/env node
+
+import chalk from 'chalk';
+import clear from 'clear';
+import program from 'commander';
+import figlet from 'figlet';
+import path from 'path';
+
+program
+    .version('0.0.1')
+    .description("An CLI to build helenjs app")
+    .option('-n, new <project-name>', 'New Helen Project')
+    .parse(process.argv);
+
+clear();
+// tslint:disable-next-line:no-console
+console.log(
+    chalk.red(figlet.textSync('helenjs', { horizontalLayout: 'full' }))
+)
+
+// tslint:disable-next-line:no-console
+console.log(program.help())
+
